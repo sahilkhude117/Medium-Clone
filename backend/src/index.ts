f7531ffa216ac55cafb501e14eb3005188e73122
sahilkhude117 <sahilkhude11@gmail.com>
2024-12-07 02:11:31 +0530

    added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,7 @@
 import { Hono } from 'hono';
-import { PrismaClient } from '@prisma/client/edge';
-import { withAccelerate } from '@prisma/extension-accelerate';
-import { sign,verify } from 'hono/jwt';
 import { userRouter } from './routes/user';
 import { blogRouter } from './routes/blog';
+import { cors } from 'hono/cors';
 
 // Create the main Hono app
 const app = new Hono<{
@@ -13,6 +11,7 @@ const app = new Hono<{
   }
 }>();
 
+app.use('/*',cors());
 app.route("/api/v1/user", userRouter);
 app.route("/api/v1/blog", blogRouter);
 
