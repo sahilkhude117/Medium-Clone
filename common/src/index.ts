cb0de8d77d893986d4534491a6c8859e1964dcf5
sahilkhude117 <sahilkhude11@gmail.com>
2024-12-06 22:32:12 +0530

    added zop types

---
+++ common/src/index.ts
@@ -0,0 +1,31 @@
+import z from 'zod';
+
+export const signupInput = z.object({
+  email : z.string().email(),
+  password : z.string().min(6),
+  name : z.string().optional()
+})
+
+export const signinInput = z.object({
+    email : z.string().email(),
+    passwaor: z.string().min(6)
+})
+
+export const createBlog = z.object({
+    title : z.string(),
+    content : z.string()
+})
+
+export const updateBlog = z.object({
+    title : z.string(),
+    content : z.string(),
+    id : z.string()
+})
+
+
+export type SignupInput = z.infer<typeof signupInput>
+export type SigninInput = z.infer<typeof signinInput>
+export type CreateBlog = z.infer<typeof createBlog>
+export type UpdateBlog = z.infer<typeof updateBlog>
+
+
