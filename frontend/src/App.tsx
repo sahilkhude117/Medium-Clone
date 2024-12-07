de14d8f866dcdc3c9b51956a0f3986e110eb6cc9
sahilkhude117 <sahilkhude11@gmail.com>
2024-12-07 18:11:37 +0530

    updated app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import './App.css'
 import { Signup } from './pages/Signup'
 import { Signin } from './pages/Signin'
 import { Blog } from './pages/Blog'
+import { Blogs } from './pages/Blogs'
+import { Publish } from './pages/Publish'
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
           <Route path='/signup' element={<Signup/>}/>
           <Route path='/signin' element={<Signin/>}/>
           <Route path='/blog' element={<Blog/>}/>
+          <Route path='/blogs' element={<Blogs/>}/>
+          <Route path='/publish' element={<Publish/>}/>
         </Routes> 
       </BrowserRouter>
     </>
