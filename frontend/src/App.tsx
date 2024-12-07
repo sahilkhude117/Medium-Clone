9489d91feecdcd003622fe63b7c6e51a66378da9
sahilkhude117 <sahilkhude11@gmail.com>
2024-12-07 22:06:43 +0530

    added :id to blog route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           <Route path='/signup' element={<Signup/>}/>
           <Route path='/signin' element={<Signin/>}/>
-          <Route path='/blog' element={<Blog/>}/>
+          <Route path='/blog/:id' element={<Blog/>}/>
           <Route path='/blogs' element={<Blogs/>}/>
           <Route path='/publish' element={<Publish/>}/>
         </Routes> 
